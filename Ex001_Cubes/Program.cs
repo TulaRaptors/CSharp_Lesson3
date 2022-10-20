e5e04b28becbeda33a981a23829ac773ed4df0b6
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 11:43:14 +0300

    Кубы чисел от 1 до N

---
+++ Ex001_Cubes/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i < n; i++)
+{
+    Console.WriteLine($"{i} в кубе = {i * i * i}");
+}
