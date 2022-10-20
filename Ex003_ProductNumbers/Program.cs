c91c16f358e6ab4fdcde5e0148cafaa145e5d9ff
Sergey_Podgornov <psv_23@mail.ru>
2022-10-20 12:38:47 +0300

    Произведение чисел от 1 до N

---
+++ Ex003_ProductNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int pr = 1;
+for (int i = 1; i <= n; i++)
+{
+    pr = pr * i;
+}
+Console.WriteLine($"Произдеведение чисел от 1 до {n} = {pr}");
